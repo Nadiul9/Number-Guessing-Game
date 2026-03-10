41058ae212f9d96d4f22238246142f0bb0c17620
Nadiul <nadiulislam614@gmail.com>
2026-03-10 18:16:08 +0530

    day 1 - Number Guessing Game

---
+++ game.py
@@ -0,0 +1,18 @@
+import random  
+def guessing_game():
+    secret = random.randint(1, 100) 
+    attempts = 0
+    print("Guess the number between 1 and 100!")
+
+    while True:
+        guess = int(input("Your guess: "))
+        attempts += 1
+        if guess < secret:
+            print("Too low! Try again.")
+        elif guess > secret:
+            print("Too high! Try again.")
+        else:
+            print(f"Correct! You got it in {attempts} attempts.")
+            break  
+
+guessing_game()  
